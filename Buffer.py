95729bcf81dc8c128f9421519dd97f92385ed673
cor22982 <cor22982@uvg.edu.gt>
2025-01-26 17:13:06 -0600

    feat: Avance buffer

--- Buffer.py
+++ Buffer.py
@@ -1,3 +1,7 @@
+AVANCE =0
+INICIOLEXEMA =0
+LEXEMAS = []
+
 # Código base para iniciar
 def cargar_buffer(entrada, inicio, tamano_buffer):
   buffer = entrada[inicio:inicio + tamano_buffer]
@@ -6,11 +10,52 @@ def cargar_buffer(entrada, inicio, tamano_buffer):
   return buffer
 
 def procesar_buffer(buffer):
-  # Procesar y extraer lexemas del buffer
-  pass
+  global AVANCE, INICIOLEXEMA, LEXEMAS
+  AVANCE = 0
+  INICIOLEXEMA = 0
+  lexema = ""
+  for i in range(len(buffer)) :
+    caracter = buffer[AVANCE]
+    if caracter == " " and AVANCE!=0:
+      INICIOLEXEMA = i + 1
+      LEXEMAS.append(lexema)
+      AVANCE = INICIOLEXEMA
+      lexema = ""
+    
+    else:
+      AVANCE = i +1
+      if caracter != " ":
+        lexema += caracter
+        if AVANCE == len(buffer):
+          LEXEMAS.append(lexema)
 
-entrada = list("Esto es un ejemplo eof")
-inicio = 0
-tamano_buffer = 10
-buffer = cargar_buffer(entrada, inicio, tamano_buffer)
-procesar_buffer(buffer)
+# buffer = cargar_buffer(entrada, inicio, tamano_buffer)
+# print(buffer)
+# procesar_buffer(buffer)
+
+if __name__ == "__main__":
+
+  BUFFER1 = []
+  BUFFER2 = []
+  tamano_buffer = 10
+  entrada = list("Esto es un ejemplo de entrada con eof")
+
+  print(len(entrada))
+  # buffer = cargar_buffer(entrada, inicio, tamano_buffer)
+  # print(buffer)
+  # procesar_buffer(buffer)
+  # print(LEXEMAS)
+  # print(AVANCE)
+  while AVANCE != len(entrada):
+    if AVANCE == 0:
+      BUFFER1 = cargar_buffer(entrada, INICIOLEXEMA, tamano_buffer)
+      procesar_buffer(BUFFER1)
+      INICIOLEXEMA = len(BUFFER1)
+    elif AVANCE== len(BUFFER1):
+      BUFFER2 = cargar_buffer(entrada, INICIOLEXEMA, tamano_buffer)
+      procesar_buffer(BUFFER2)
+    elif AVANCE == len(BUFFER2):
+      BUFFER1 = cargar_buffer(entrada, INICIOLEXEMA, tamano_buffer)
+      procesar_buffer(BUFFER1)
+  
+  
