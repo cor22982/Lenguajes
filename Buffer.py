05fcbafcb22d853e3c81b8f84b932589ffcac156
cor22982 <cor22982@uvg.edu.gt>
2025-01-26 18:20:17 -0600

    feat: Completado totalmente

--- Buffer.py
+++ Buffer.py
@@ -4,6 +4,7 @@ LEXEMAS = []
 LECTOR =0
 LEXEMA = ""
 FLAG_BUFFER = 1
+FLAG_SALIDA = True
 
 # Código base para iniciar
 def cargar_buffer(entrada, inicio, tamano_buffer):
@@ -13,7 +14,7 @@ def cargar_buffer(entrada, inicio, tamano_buffer):
   return buffer
 
 def procesar_buffer(buffer):
-  global AVANCE, INICIOLEXEMA, LEXEMAS, LEXEMA
+  global AVANCE, INICIOLEXEMA, LEXEMAS, LEXEMA, FLAG_SALIDA
   AVANCE = 0
   INICIOLEXEMA = 0
   for i in range(len(buffer)) :
@@ -24,31 +25,23 @@ def procesar_buffer(buffer):
       AVANCE = INICIOLEXEMA
       LEXEMA = ""
     
+    elif caracter =="eof":
+      LEXEMAS.append(LEXEMA)
+      FLAG_SALIDA = False
+      break
+
     else:
       AVANCE = i +1
       if caracter != " ":
         LEXEMA += caracter
 
-# buffer = cargar_buffer(entrada, inicio, tamano_buffer)
-# print(buffer)
-# procesar_buffer(buffer)
-
 if __name__ == "__main__":
-
   BUFFER1 = []
   BUFFER2 = []
   tamano_buffer = 10
   entrada = list("Esto es un ejemplo de entrada con eof")
 
-  print(len(entrada))
-  # buffer = cargar_buffer(entrada, inicio, tamano_buffer)
-  # print(buffer)
-  # procesar_buffer(buffer)
-  # print(LEXEMAS)
-  buffer = cargar_buffer(entrada,20, tamano_buffer)
-  print(buffer)
-  # print(AVANCE)
-  while LECTOR != len(entrada):
+  while FLAG_SALIDA:
     if AVANCE == 0 and FLAG_BUFFER ==1:
       BUFFER1 = cargar_buffer(entrada, LECTOR, tamano_buffer)
       procesar_buffer(BUFFER1)
@@ -63,5 +56,7 @@ if __name__ == "__main__":
       procesar_buffer(BUFFER1)
       LECTOR += len(BUFFER1)
       FLAG_BUFFER = 1
+  
+  print(LEXEMAS)
   
   
